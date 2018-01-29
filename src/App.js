6b2841c65e5398c1e3077dbbe6324c3e1b5c186a
Diego Diaz de Berenguer <d.diazdeberenguer@gmail.com>
2018-01-29 13:27:27 -0600

    Add Bootstrap and some CSS styling

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { Button } from 'react-bootstrap';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container-fluid" id="main-container">
+        <aside className="user-list">
+          Online Users
+        </aside>
+        <section className="main">
+          <section className="messages">
+            Messages
+          </section>
+        </section>
+        <section className="newMessage" id="new">
+          <Button bsStyle="primary" block>Send</Button>
+        </section>
       </div>
     );
   }
